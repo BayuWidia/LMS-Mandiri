290aa956c03d99dc68b619f76bb6a8fb0905f433
BWS <bayuwidiasantoso@gmail.com>
2018-02-26 13:25:53 +0700

    test

--- src/main/java/com/mandiri/configuration/SecurityConfiguration.java
+++ src/main/java/com/mandiri/configuration/SecurityConfiguration.java
@@ -8,13 +8,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.boot.autoconfigure.security.oauth2.resource.PrincipalExtractor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.encoding.Md5PasswordEncoder;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import com.mandiri.repository.UserProfileRepository;
@@ -26,6 +29,9 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	@Autowired
 	private BCryptPasswordEncoder bCryptPasswordEncoder;
+	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
 
 	@Autowired
 	private DataSource dataSource;
@@ -45,9 +51,22 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.jdbcAuthentication().usersByUsernameQuery(usersQuery).authoritiesByUsernameQuery(rolesQuery)
-				.dataSource(dataSource).passwordEncoder(bCryptPasswordEncoder);
+//				.dataSource(dataSource).passwordEncoder(bCryptPasswordEncoder);
+				.dataSource(dataSource).passwordEncoder(passwordEncoder());
 	}
+	
+//	@Bean
+//	public PasswordEncoder passwordEncoder(){
+//	    //implements PasswordEncoder and overide encode method with the MD5 protocol
+//	    return (PasswordEncoder) new MD5PasswordEncoder();
+//	}
 
+	@Bean
+    public PasswordEncoder passwordEncoder(){
+        PasswordEncoder encoder = (PasswordEncoder) new Md5PasswordEncoder();
+        return encoder;
+    }
+	 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 
